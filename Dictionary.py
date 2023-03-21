e3df62cf255bc27c41ee9fefc469e97856d73ae2
Rakshith-0007 <120350251+Rakshith-0007@users.noreply.github.com>
2023-03-21 10:01:14 +0530

    Add files via upload

---
+++ Dictionary.py
@@ -0,0 +1,20 @@
+a={"Rakshith":"Coder","pratham":"Cricketer","Akash":"Anime","Prajwal":"Topper","Mutable":"Can be Changed"}
+op=input("Enter the Meaning that should be Found:")
+
+if op=="Rakshith":
+    print("Your Word Meaning is\n ",a["Rakshith"])
+
+elif op=="pratham":
+    print("Your Word Meaning is\n ", a["pratham"])
+
+elif op=="Akash":
+    print("Your Word Meaning is\n ", a["Akash"])
+
+elif op=="Prajwal":
+    print("Your Word Meaning is\n ", a["Prajwal"])
+
+elif op == "Mutable":
+    print("Your Word Meaning is\n ", a["Mutable"])
+
+else:
+    print("Invalid Entry")
